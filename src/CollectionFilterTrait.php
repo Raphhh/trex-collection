b7a8b5b81e91f2f885718643b1deae84ccf7d064
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2016-02-16 13:36:00 +0100

    fix array_filter behavior according to hhvm
    even if php is 5.6, hhvm 3.6.1 does not accept 3 args

--- src/CollectionFilterTrait.php
+++ src/CollectionFilterTrait.php
@@ -10,8 +10,14 @@ trait CollectionFilterTrait
      */
     public function filter(callable $filter = null, $flag = 0)
     {
-        if (version_compare(phpversion(), '5.6.0', '>=')) {
-            return new $this(array_filter((array)$this, $filter, $flag));
+        if (defined('HHVM_VERSION')) {
+            if (version_compare(HHVM_VERSION, '3.8.1', '>=')) {
+                return new $this(array_filter((array)$this, $filter, $flag));
+            }
+        } else {
+            if (version_compare(phpversion(), '5.6.0', '>=')) {
+                return new $this(array_filter((array)$this, $filter, $flag));
+            }
         }
         return new $this(array_filter((array)$this, $filter));
     }
@@ -27,7 +33,7 @@ trait CollectionFilterTrait
     public function each(callable $callback, array $args = [])
     {
         return new $this(array_map(
-            function($value) use($callback, $args){
+            function ($value) use ($callback, $args) {
                 return call_user_func($callback, $value, $args) ?: $value;
             },
             (array)$this
@@ -45,7 +51,7 @@ trait CollectionFilterTrait
      */
     public function extract($startIndex, $length = 0, $areKeysPreserved = true)
     {
-        $collection = (array) $this;
+        $collection = (array)$this;
         $length = $length ?: count($collection);
         return new $this(array_slice($collection, $startIndex, $length, $areKeysPreserved));
     }
