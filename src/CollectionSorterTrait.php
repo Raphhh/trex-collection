26f7c4b8153da9d78ce3eb622f551a6078fa932a
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2016-06-08 18:17:35 +0200

    comment

--- src/CollectionSorterTrait.php
+++ src/CollectionSorterTrait.php
@@ -4,6 +4,8 @@ namespace TRex\Collection;
 trait CollectionSorterTrait
 {
     /**
+     * Reindex the keys
+     *
      * @return $this
      */
     public function reindex()
@@ -23,6 +25,8 @@ trait CollectionSorterTrait
     }
 
     /**
+     * Reverses the values
+     *
      * @param bool $areKeysPreserved
      * @return $this
      */
@@ -32,6 +36,8 @@ trait CollectionSorterTrait
     }
 
     /**
+     * Shuffles the values
+     *
      * @param bool $areKeysPreserved
      * @return $this
      */
@@ -49,6 +55,17 @@ trait CollectionSorterTrait
     }
 
     /**
+     * Splits the values into every possible response returned by the callback.
+     *
+     * For example, if the callback juste return the value:
+     * ['a', 'b', 'c']
+     * will become:
+     * ['a' => ['a'], 'b' => ['b'], 'c' => ['c']]
+     *
+     * If the callback return a boolean, it will be return an array with two collections.
+     * [0 => $nonMatchedCollection, 1 => $matchedCollection]
+     * This seems to be quite the same as Doctrine's ARrayCollection. But be careful because keys are inverted.
+     *
      * @param callable $callback
      * @return $this[]
      */
